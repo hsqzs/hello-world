d167088a04c0b6098623f77387b84e56b302d7aa
hsqzs <58459482+hsqzs@users.noreply.github.com>
2019-12-03 15:12:52 +0800

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <curl/curl.h>
+
+int main(void)
+{
+CURL *curl;CURLcode res ;
+
+curl=curl_ easy_ init();if(curl){
+
+curl_ easy_ setopt(curl,CURLOPT_ URL, "http://myip. ipip.net/");
+res=curl_ easy_ perform(curl);if(res!=CURLE_ OK)
+fprintf(stderr, "curl_ _easy_ perform() failed:%sIn",
+curl_ easy_ strerror(res));curl_ easy_ cleanup(curl);
+}
+return0;
+}
